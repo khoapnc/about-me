8f001c2932eef5f4aea0145a42919f0c14a44f78
khoapnc <khoapnc@unit.com.vn>
2025-03-21 17:40:28 +0700

    Install Zustand and shadcn/ui with dark mode support

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <ModeToggle />
+      <Button>BTN</Button>
+    </div>
+  );
+}
